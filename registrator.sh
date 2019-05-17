822ff5d38b97cd1bd2d21019d9c3fab9d966348e
yangjunhai <yangjunhai@xindong.com>
2019-05-17 20:46:27 +0800

    etcd 3 lease 优化，同一个物理上使用同一个租期

--- registrator.sh
+++ registrator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ip=172.26.148.189
-tag=$(cat VERSION)
+ip=172.26.147.228
+tag=$(grep "^version" docker_run.config | awk -F'=' '{print $2}')
 id=myRegistrator
-dockerRun=registrator:${tag}
+dockerRun=qa.haidao:5000/registrator:${tag}
 
 docker stop $id && docker rm $id && echo "stop and remove $id"
 docker run -d  --name=$id  --net=host  --volume=/var/run/docker.sock:/tmp/docker.sock  $dockerRun  \
